409bb5069ffdf6f1ecf4d71e32c6d546490b05b6
stephchen817 <66129328+stephchen817@users.noreply.github.com>
2020-06-03 02:35:54 +0800

    UI

---
+++ client/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Login from './App';
+import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Login />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+serviceWorker.unregister();
