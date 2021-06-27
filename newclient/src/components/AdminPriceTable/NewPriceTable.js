4a90da418cfae54bbde65d53ccd276326b96cbdc
JerickWong <jerick_wong@dlsu.edu.ph>
2021-06-28 00:23:53 +0800

    fix: edit issue

--- newclient/src/components/AdminPriceTable/NewPriceTable.js
+++ newclient/src/components/AdminPriceTable/NewPriceTable.js
@@ -34,28 +34,24 @@ export default function NewPriceTable(props) {
   })
 
   useEffect(() => {
-    setCategory(props.data);
     const temp = []
-    props.data.items.map(() => {
+    const tempEdit = []
+
+    props.data.items.map(item => {
       temp.push(false)
+      tempEdit.push({...item})
     })
+    
     if (props.isAdding)
       temp[temp.length-1] = true
-    setIsEditing(temp)
 
+    setIsEditing(temp)
+    setEdit(tempEdit)
+    setCategory(props.data);
   }, [props.data, props.isAdding]);
 
   function editItem(event, item, index) {
-    setEdit({
-      unit: item.unit,
-      product_name: item.product_name,
-      brand_name: item.brand_name,
-      model_name: item.model_name,
-      list_price: item.list_price,
-      price_adjustment: item.price_adjustment,
-      net_price: item.net_price,
-      remarks: item.remarks,
-    })
+    
     let tempEditing = [...isEditing]
     tempEditing[index] = true
     setIsEditing(tempEditing)
@@ -65,7 +61,7 @@ export default function NewPriceTable(props) {
     if (props.isAdding && index === category.items.length-1) 
       addItem()
     else {
-      category.items[index] = edit
+      category.items[index] = edit[index]
 
       if (props.isAdding) {
         category.items.pop()
@@ -130,14 +126,14 @@ export default function NewPriceTable(props) {
           category.items.map((cat, index) => {
             return (
               <tr key={cat._id}>
-                <td>{isEditing[index] ? <Form.Control onChange={e => setEdit({...edit, unit: e.target.value})} type="text" size="sm" defaultValue={cat.unit ? cat.unit.slice() : ''}/> : cat.unit}</td>
-                <td>{isEditing[index] ? <Form.Control onChange={e => setEdit({...edit, product_name: e.target.value})} type="text" size="sm" defaultValue={cat.product_name ? cat.product_name.slice() : ''}/> : cat.product_name}</td>
-                <td>{isEditing[index] ? <Form.Control onChange={e => setEdit({...edit, brand_name: e.target.value})} type="text" size="sm" defaultValue={cat.brand_name ? cat.brand_name.slice() : ''}/> : cat.brand_name}</td>
-                <td>{isEditing[index] ? <Form.Control onChange={e => setEdit({...edit, model_name: e.target.value})} type="text" size="sm" defaultValue={cat.model_name ? cat.model_name.slice() : ''}/> : cat.model_name}</td>
-                <td>{isEditing[index] ? <Form.Control onChange={e => setEdit({...edit, list_price: e.target.value})} type="text" size="sm" defaultValue={cat.list_price ? cat.list_price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",").slice() : ''}/> : cat.list_price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</td>
-                <td>{isEditing[index] ? <Form.Control onChange={e => setEdit({...edit, price_adjustment: e.target.value})} type="text" size="sm" defaultValue={cat.price_adjustment ? cat.price_adjustment.slice() : ''}/> : cat.price_adjustment ? cat.price_adjustment : ''}</td>
-                <td>{isEditing[index] ? <Form.Control onChange={e => setEdit({...edit, net_price: e.target.value})} type="text" size="sm" defaultValue={cat.net_price ? cat.net_price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",").slice() : ''}/> : cat.net_price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</td>
-                <td>{isEditing[index] ? <Form.Control onChange={e => setEdit({...edit, remarks: e.target.value})} type="text" size="sm" defaultValue={cat.remarks ? cat.remarks.slice() : ''}/> : cat.remarks}</td>
+                <td>{isEditing[index] ? <Form.Control onChange={e => edit[index].unit = e.target.value} type="text" size="sm" defaultValue={cat.unit ? cat.unit.slice() : ''}/> : cat.unit}</td>
+                <td>{isEditing[index] ? <Form.Control onChange={e => edit[index].product_name = e.target.value} type="text" size="sm" defaultValue={cat.product_name ? cat.product_name.slice() : ''}/> : cat.product_name}</td>
+                <td>{isEditing[index] ? <Form.Control onChange={e => edit[index].brand_name = e.target.value} type="text" size="sm" defaultValue={cat.brand_name ? cat.brand_name.slice() : ''}/> : cat.brand_name}</td>
+                <td>{isEditing[index] ? <Form.Control onChange={e => edit[index].model_name = e.target.value} type="text" size="sm" defaultValue={cat.model_name ? cat.model_name.slice() : ''}/> : cat.model_name}</td>
+                <td>{isEditing[index] ? <Form.Control onChange={e => edit[index].list_price = e.target.value} type="text" size="sm" defaultValue={cat.list_price ? cat.list_price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",").slice() : ''}/> : cat.list_price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</td>
+                <td>{isEditing[index] ? <Form.Control onChange={e => edit[index].price_adjustment = e.target.value} type="text" size="sm" defaultValue={cat.price_adjustment ? cat.price_adjustment.slice() : ''}/> : cat.price_adjustment ? cat.price_adjustment : ''}</td>
+                <td>{isEditing[index] ? <Form.Control onChange={e => edit[index].net_price = e.target.value} type="text" size="sm" defaultValue={cat.net_price ? cat.net_price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",").slice() : ''}/> : cat.net_price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</td>
+                <td>{isEditing[index] ? <Form.Control onChange={e => edit[index].remarks = e.target.value} type="text" size="sm" defaultValue={cat.remarks ? cat.remarks.slice() : ''}/> : cat.remarks}</td>
                 <td>
                   {
                     category.name === "Pending Items" && !isEditing[index] &&
